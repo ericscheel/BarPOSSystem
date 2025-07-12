05fd3b3db7e3af50039c65492ee03d8e651f84f1
Scheel, Eric <eric.scheel@datagroup.de>
2025-07-12 17:08:01 +0000

    Füge JUnit 5 Abhängigkeit im Modul-Info hinzu

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module barpos {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires org.junit.jupiter.api; // Für JUnit 5 Tests
     
     exports com.barpos;
 }
